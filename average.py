7fcbf131fe54f90a7bcdc959fa82106930175153
sviatoslav-zapolskyi <sviatoslav.zapolskyi@gmail.com>
2026-03-15 10:58:00 +0000

    приклади

---
+++ average.py
@@ -0,0 +1,9 @@
+a, b, c = map(float, 
+              input('Введіть a, b, c: ')
+                 .split()
+             )
+
+total = a + b + c
+average = total / 3
+
+print('Середнє арифметичне:', average)
---
+++ turtle-hello-world.py
@@ -0,0 +1,6 @@
+import turtle
+
+t = turtle.Turtle()
+t.forward(100)  # намалювати лінію довжиною 100
+
+turtle.done()
