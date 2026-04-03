981e86a3ff966d55f0ad87e6da7107791179faf5
sviatoslav-zapolskyi <sviatoslav.zapolskyi@gmail.com>
2026-04-03 10:47:03 +0000

    Exercise 5

--- work21.py
+++ work21.py
@@ -1,2 +1,11 @@
-a = input('a = ? ')
-a = ? 5
+print("what is you'r name?")
+name = input()
+
+print("how old are you?")
+age = input()
+
+
+print('what is your grade?')
+form = input()
+
+print('Hello', name, '!', 'You are', age, 'years', 'You study at', form, 'grade')
